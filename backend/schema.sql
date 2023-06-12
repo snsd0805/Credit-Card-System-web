a7ce5cfd7e4584fb248ec06c9dd47906b8c76a41
snsd0805 <levi900227@gmail.com>
2023-06-12 19:41:00 +0800

    fix: change the product's code in database schema

--- backend/schema.sql
+++ backend/schema.sql
@@ -46,7 +46,8 @@ CREATE TABLE "order_products" (
 INSERT INTO "main"."shops"("id","address","name") VALUES (NULL,"0x73D081e82b35D6E6f9f5e72EBBB6b637d4f46992","全家便利商店");
 INSERT INTO "main"."shops"("id","address","name") VALUES (NULL,"0xDa68136fcB885a2ec44db6dcE946F656aF457A76","暨大圖文部");
 
-INSERT INTO "main"."products"("id","shop_id","name","code", "price") VALUES (NULL,1,"濃辛咖哩飯","7233957360139", "10000000000000000");
-INSERT INTO "main"."products"("id","shop_id","name","code", "price") VALUES (NULL,1,"霜淇淋","4718022345288", "5000000000000000");
-INSERT INTO "main"."products"("id","shop_id","name","code", "price") VALUES (NULL,2,"證件套","748009345271", "25000000000000000");
-INSERT INTO "main"."products"("id","shop_id","name","code", "price") VALUES (NULL,2,"便條紙","4979274503226", "4000000000000000");
+INSERT INTO "main"."products"("id","shop_id","name","code", "price") VALUES (NULL,1,"悠遊卡","7233957360139", "10000000000000000");
+INSERT INTO "main"."products"("id","shop_id","name","code", "price") VALUES (NULL,1,"退燒藥","4712521005896", "25000000000000000");
+INSERT INTO "main"."products"("id","shop_id","name","code", "price") VALUES (NULL,1,"小酒杯","8809706849058", "50000000000000000");
+INSERT INTO "main"."products"("id","shop_id","name","code", "price") VALUES (NULL,2,"便條紙","4979274503226",  "7000000000000000");
+INSERT INTO "main"."products"("id","shop_id","name","code", "price") VALUES (NULL,2,"電池","4904530591785",   "30000000000000000");
